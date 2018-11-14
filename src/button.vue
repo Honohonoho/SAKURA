10f9d000ec89116139e52a1f5fc68405a066b686
Honohonoho <751718620@qq.com>
2018-11-14 23:55:57 +0800

    给 按钮添加 icon 并且可以指定按钮类型和位置

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,15 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg class="icon" v-if="icon"><use :xlink:href="`#i-${icon}`"></use></svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 
 <script>
 export default {
-    name: 'g-button'
+    props: ['icon', 'iconPosition']
 }
 </script>
 
@@ -14,8 +19,11 @@ export default {
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--border-radius);
-        border: 1px solid var(--border-color);
+        border: 1px  solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
         &:hover {
             border-color: var(--border-color-hover);
         }
@@ -24,6 +32,20 @@ export default {
         }
         &:focus {
             outline: none;
+        }
+        > .icon {
+            order: 1;
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2;
+            }
+            > .content {
+                order: 1;
+            }
         }
     }
 </style>
