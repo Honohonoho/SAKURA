428e6096e6ce70ce24a7f5fc645f5b0ff164730b
Honohonoho <751718620@qq.com>
2018-11-17 23:02:10 +0800

    添加click事件，切换loading和正常状态的图标显示

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,8 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-        <g-icon class="icon loading" v-if="icon" :name="'loading'"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}"
+        @click="$emit('click')">
+        <g-icon class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon class="icon loading" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -12,6 +13,10 @@
 export default {
     props: {
         icon: {},
+        loading: {
+            type: Boolean,
+            default: false
+        },
         iconPosition: {
             type: String,
             default: 'left',
