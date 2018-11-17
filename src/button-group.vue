42581e706a9dd24df6f6a0c02069c856be25da0d
Honohonoho <751718620@qq.com>
2018-11-17 23:36:55 +0800

    添加 button-group 组件， 用于显示一组按钮

---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+.g-button-group {
+    display: inline-flex;
+    vertical-align: top;
+    > .g-button {
+        border-radius: 0;
+        margin-left: -1px; // 去除边框重叠
+        &:hover {
+            position: relative;
+            z-index: 1;
+        }
+        &:first-child {
+            border-top-left-radius: var(--border-radius);
+            border-bottom-left-radius: var(--border-radius);
+        }
+        &:last-child {
+            border-top-right-radius: var(--border-radius);
+            border-bottom-right-radius: var(--border-radius);
+        } 
+    }
+}
+</style>
+
