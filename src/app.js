e58b9a64e910916ce015bbaf0ea3668fa3cdcc81
Honohonoho <751718620@qq.com>
2018-11-18 21:39:18 +0800

    使用 chai-spies 监听click的回调函数

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ new Vue({
 });
 
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 
 const expect = chai.expect
 // 单元测试
@@ -24,83 +26,81 @@ const expect = chai.expect
 // 测试图标
 {
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings'
         }
     })
-    button.$mount() // 也可以4mount('某个元素')
-    let useElement = button.$el.querySelector('use')
+    vm.$mount() // 也可以4mount('某个元素')
+    let useElement = vm.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#i-settings')
     // 通过就自毁
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
 // 测试loading
 {
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings',
             loading: true
         }
     })
-    button.$mount()
-    let useElement = button.$el.querySelector('use')
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
     let href = useElement.getAttribute('xlink:href')
     expect(href).to.eq('#i-loading')
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
 // 测试图标左右位置
 {   
     const div = document.createElement('div')
     document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings'
         }
     })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
     let order = window.getComputedStyle(svg).order
     expect(order).to.eq('1') // css 属性值都为String
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
 {   
     const div = document.createElement('div')
     document.body.appendChild(div)
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings',
             iconPosition: 'right'
         }
     })
-    button.$mount(div)
-    let svg = button.$el.querySelector('svg')
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
     let order = window.getComputedStyle(svg).order
     expect(order).to.eq('2') // css 属性值都为String
-    button.$el.remove()
-    button.$destroy()
+    vm.$el.remove()
+    vm.$destroy()
 }
 // 测试事件监听
 {   
     const Constructor = Vue.extend(Button)
-    const gButton = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings',
         }
     })
-    gButton.$mount()
-    gButton.$on('click', function () {
-        console.log(21)
-    })
-    let button = gButton.$el
+    vm.$mount()
+    let spy = chai.spy(function(){})
+    vm.$on('click', spy) // 希望 click 时，spy函数执行
+    let button = vm.$el
     button.click()
-    button.remove()
-    gButton.$destroy()
+    expect(spy).to.have.been.called()
 }
