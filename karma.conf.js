35fd87965179303d116026fc759554c9dee07de6
Honohonoho <751718620@qq.com>
2018-11-22 21:44:11 +0800

    删除 旧测试用例代码

--- karma.conf.js
+++ karma.conf.js
@@ -54,7 +54,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['ChromeHeadless'],
+        browsers: ['ChromeHeadless'], // use ChromeHeadless not Chrome
 
 
         // Continuous Integration mode
