da424bb335c1d156037db4955aadfa89fb233bc1
Honohonoho <751718620@qq.com>
2018-11-24 17:27:09 +0800

    新建一个空 input 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Button from './button';
 import ButtonGroup from './button-group';
+import Input from './input';
 
 Vue.component('g-button', Button);
 Vue.component('g-button-group', ButtonGroup);
+Vue.component('g-input', Input);
 
 new Vue({
     el: '#app',
