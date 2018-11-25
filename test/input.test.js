5d0a71597f963d4bbcab5108b469df5e510fc0a5
Honohonoho <751718620@qq.com>
2018-11-25 16:55:39 +0800

    优化 input 单元测试，把属性和事件测试分组

--- test/input.test.js
+++ test/input.test.js
@@ -13,50 +13,66 @@ describe('Input', () => {
     it('input exist.', () => {
         expect(Input).to.exist
     })
-    it('accpet property: value', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-            propsData: {
-                value: '1234'
-            }
-        }).$mount()
-        const inputElement = vm.$el.querySelector('input')
-        expect(inputElement.value).to.equal('1234')
-        vm.$destroy()
+    describe('accpet props', () => {
+        it('accpet property: value', () => {
+            const Constructor = Vue.extend(Input)
+            const vm = new Constructor({
+                propsData: {
+                    value: '1234'
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.value).to.equal('1234')
+            vm.$destroy()
+        })
+        it('accpet property: disabled', () => {
+            const Constructor = Vue.extend(Input)
+            const vm = new Constructor({
+                propsData: {
+                    disabled: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.disabled).to.equal(true)
+            vm.$destroy()
+        })
+        it('accpet property: readonly', () => {
+            const Constructor = Vue.extend(Input)
+            const vm = new Constructor({
+                propsData: {
+                    readonly: true
+                }
+            }).$mount()
+            const inputElement = vm.$el.querySelector('input')
+            expect(inputElement.readOnly).to.equal(true) // use lower camel case, 'readonly' change to 'readOnly'
+            vm.$destroy()
+        })
+        it('accpet property: error', () => {
+            const Constructor = Vue.extend(Input)
+            const vm = new Constructor({
+                propsData: {
+                    error: '输入有误'
+                }
+            }).$mount()
+            const useElement = vm.$el.querySelector('use')
+            expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+            const errorMessage = vm.$el.querySelector('.error-message')
+            expect(errorMessage.innerText).to.equal('输入有误')
+            vm.$destroy()
+        })
     })
-    it('accpet property: disabled', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-            propsData: {
-                disabled: true
-            }
-        }).$mount()
-        const inputElement = vm.$el.querySelector('input')
-        expect(inputElement.disabled).to.equal(true)
-        vm.$destroy()
-    })
-    it('accpet property: readonly', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-            propsData: {
-                readonly: true
-            }
-        }).$mount()
-        const inputElement = vm.$el.querySelector('input')
-        expect(inputElement.readOnly).to.equal(true) // use lower camel case, 'readonly' change to 'readOnly'
-        vm.$destroy()
-    })
-    it('accpet property: error', () => {
-        const Constructor = Vue.extend(Input)
-        const vm = new Constructor({
-            propsData: {
-                error: '输入有误'
-            }
-        }).$mount()
-        const useElement = vm.$el.querySelector('use')
-        expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
-        const errorMessage = vm.$el.querySelector('.error-message')
-        expect(errorMessage.innerText).to.equal('输入有误')
-        vm.$destroy()
+    describe('event handle', () => {
+        it('event:change available', () => {
+
+        })
+        it('event:input available', () => {
+            
+        })
+        it('event:focus available', () => {
+            
+        })
+        it('event:blur available', () => {
+            
+        })
     })
 })
