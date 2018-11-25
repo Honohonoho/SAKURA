3228ba276625e4c97f6686c4b2f1f8531b6db731
Honohonoho <751718620@qq.com>
2018-11-25 16:47:17 +0800

    添加 input 组件 value 属性测试

--- test/input.test.js
+++ test/input.test.js
@@ -13,4 +13,15 @@ describe('Input', () => {
     it('input exist.', () => {
         expect(Input).to.exist
     })
+    it('accpet property: value', () => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({
+            propsData: {
+                value: '1234'
+            }
+        }).$mount()
+        const inputElement = vm.$el.querySelector('input')
+        expect(inputElement.value).to.equal('1234')
+        vm.$destroy()
+    })
 })
