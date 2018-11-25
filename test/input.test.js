d0fc4497e4ef1588d8cbb698df0e4ef8e9398504
Honohonoho <751718620@qq.com>
2018-11-25 16:49:27 +0800

    添加 input 组件 error 显示测试

--- test/input.test.js
+++ test/input.test.js
@@ -46,4 +46,17 @@ describe('Input', () => {
         expect(inputElement.readOnly).to.equal(true) // use lower camel case, 'readonly' change to 'readOnly'
         vm.$destroy()
     })
+    it('accpet property: error', () => {
+        const Constructor = Vue.extend(Input)
+        const vm = new Constructor({
+            propsData: {
+                error: '输入有误'
+            }
+        }).$mount()
+        const useElement = vm.$el.querySelector('use')
+        expect(useElement.getAttribute('xlink:href')).to.equal('#i-error')
+        const errorMessage = vm.$el.querySelector('.error-message')
+        expect(errorMessage.innerText).to.equal('输入有误')
+        vm.$destroy()
+    })
 })
