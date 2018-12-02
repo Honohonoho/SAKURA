e369c96a89e5ad959fe516ba1c6b779cf5b9ee7d
Honohonoho <751718620@qq.com>
2018-12-02 16:22:50 +0800

    添加 栅格布局测试用例：测试Col的span属性

--- test/col.test.js
+++ test/col.test.js
@@ -11,5 +11,18 @@ describe('Button', () => {
     it('存在.', () => {
         expect(Col).to.exist
     })
+    it('接受 span 属性.', () => {
+        const Constructor = Vue.extend(Col)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const vm = new Constructor({
+            propsData: {
+                span: '12'
+            }
+        }).$mount(div)
+        expect( vm.$el.classList.contains('col-12') ).to.equal(true)
+        div.remove()
+        vm.$destroy()
+    })
     
 })
