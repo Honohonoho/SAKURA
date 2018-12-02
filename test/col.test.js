c9063a459870e7ce4223e23ffdffa4862dc9aabd
Honohonoho <751718620@qq.com>
2018-12-02 16:29:31 +0800

    继续添加 栅格布局测试用例：测试 Col 的 响应式class

--- test/col.test.js
+++ test/col.test.js
@@ -51,4 +51,46 @@ describe('Button', () => {
         div.remove()
         vm.$destroy()
     })
+    it('接受 pad 属性.', () => {
+        const Constructor = Vue.extend(Col)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const vm = new Constructor({
+            propsData: {
+                pad: {span: 12, offset: 10}
+            }
+        }).$mount(div)
+        expect( vm.$el.classList.contains('col-pad-12') ).to.equal(true)
+        expect( vm.$el.classList.contains('offset-pad-10') ).to.equal(true)
+        div.remove()
+        vm.$destroy()
+    })
+    it('接受 narrow-pc 属性.', () => {
+        const Constructor = Vue.extend(Col)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const vm = new Constructor({
+            propsData: {
+                narrowPc: {span: 12, offset: 10}
+            }
+        }).$mount(div)
+        expect( vm.$el.classList.contains('col-narrow-pc-12') ).to.equal(true)
+        expect( vm.$el.classList.contains('offset-narrow-pc-10') ).to.equal(true)
+        div.remove()
+        vm.$destroy()
+    })
+    it('接受 wide-pc 属性.', () => {
+        const Constructor = Vue.extend(Col)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const vm = new Constructor({
+            propsData: {
+                widePc: {span: 12, offset: 10}
+            }
+        }).$mount(div)
+        expect( vm.$el.classList.contains('col-wide-pc-12') ).to.equal(true)
+        expect( vm.$el.classList.contains('offset-wide-pc-10') ).to.equal(true)
+        div.remove()
+        vm.$destroy()
+    })
 })
