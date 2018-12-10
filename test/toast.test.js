a39d0f429e26b7cc28789893d70e5ea1fd2d6636
Honohonoho <751718620@qq.com>
2018-12-10 21:00:27 +0800

    添加 Toast 组件基本测试用例

---
+++ test/toast.test.js
@@ -0,0 +1,12 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Toast from '../src/toast'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Toast', () => {
+    it('存在.', () => {
+        expect(Toast).to.be.exist
+    })
+})
