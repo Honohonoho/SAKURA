99d22dd300f4e9b0475ec0f16ef17d15dcc7bc83
Honohonoho <751718620@qq.com>
2018-12-11 23:40:32 +0800

    添加 Toast 组件的props属性单元测试用例

--- test/toast.test.js
+++ test/toast.test.js
@@ -9,4 +9,21 @@ describe('Toast', () => {
     it('存在.', () => {
         expect(Toast).to.be.exist
     })
+    describe('props', function() {
+        it('接受 autoClose', (done) => {
+            let div = document.createElement('div')
+            document.body.appendChild(div)
+            const Constructor = Vue.extend(Toast)
+            const vm = new Constructor({
+                propsData: {
+                    autoClose: true,
+                    autoCloseDelay: 1
+                }
+            }).$mount(div)
+            vm.$on('close', function () {
+                expect(document.body.contains(vm.$el)).to.eq(false)
+                done()
+            })
+        })
+    })
 })
