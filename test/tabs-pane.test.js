0a60e056196b371c8d00c69f5f10383865a43655
Honohonoho <751718620@qq.com>
2018-12-18 21:32:18 +0800

    添加 Tabs 组件 tabs-pane 测试用例

---
+++ test/tabs-pane.test.js
@@ -0,0 +1,13 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import TabsPane from '../src/tabs-pane';
+// Vue.component('g-tabs-pane', TabsPane);
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('tabs-pane', () => {
+    it('存在.', () => {
+        expect(TabsPane).to.exist
+    })
+})
