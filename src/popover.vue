7313ce06bd9e0b60d5f0d843634e521064080100
Honohonoho <751718620@qq.com>
2018-12-20 21:28:21 +0800

    重构 Popover 组件 当组件父元素有overflow:hidden时的bug，改变组件实现方式

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 <script>
@@ -14,11 +16,18 @@ export default {
             visible: false
         }
     },
+    mounted(){
+
+    },
     methods: {
         xxx() {
             this.visible = !this.visible
             if(this.visible === true) {
                 this.$nextTick(()=> {
+                    document.body.appendChild(this.$refs.contentWrapper)
+                    let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                    this.$refs.contentWrapper.style.left = left + 'px'
+                    this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                     let eventHandler = ()=> {
                         this.visible = false
                         document.removeEventListener('click', eventHandler)
@@ -26,9 +35,8 @@ export default {
                     document.addEventListener('click', eventHandler)
                 })
             }
-        }
+        },
     }
-    
 }
 </script>
 <style lang="scss">
@@ -36,13 +44,11 @@ export default {
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-        position: absolute;
-        bottom: 100%;
-        left: 0;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
 }
-
+.content-wrapper {
+    position: absolute;
+    transform: translateY(-100%);
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+}
 </style>
