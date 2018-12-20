6389e809648781c58aa43b32ea44780d5a606dc5
Honohonoho <751718620@qq.com>
2018-12-20 21:29:13 +0800

    优化 Popover 组件 当组件父元素有overflow:hidden时的bug，改变组件实现方式

--- src/popover.vue
+++ src/popover.vue
@@ -26,7 +26,7 @@ export default {
                 this.$nextTick(()=> {
                     document.body.appendChild(this.$refs.contentWrapper)
                     let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                    this.$refs.contentWrapper.style.left = left + 'px'
+                    this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
                     this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
                     let eventHandler = ()=> {
                         this.visible = false
