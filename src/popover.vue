f369180314c3b44b93949884b0ecb640e39191e6
Honohonoho <751718620@qq.com>
2018-12-21 21:50:03 +0800

    修复 Popover 组件阻止事件冒泡的bug & 重构代码

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="popover" @click="onClick">
+    <div class="popover" ref="popover" @click="onClick">
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
@@ -22,24 +22,39 @@ export default {
     methods: {
         onClick(event) {
             if (this.$refs.triggerWrapper.contains(event.target)) {
-                this.visible = !this.visible
                 if(this.visible === true) {
-                    this.$nextTick(()=> {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-                        let eventHandler = (e)=> {
-                            if (!this.$refs.contentWrapper.contains(e.target)) {
-                                this.visible = false
-                                document.removeEventListener('click', eventHandler)
-                            }
-                        }
-                        document.addEventListener('click', eventHandler)
-                    })
+                    this.closeContentAndRemoveListener()
+                }else {
+                    this.showContentNextTick()
                 }
             }
         },
+        closeContentAndRemoveListener() {
+            this.visible = false
+            document.removeEventListener('click', this.onClickDocument)
+        },
+        showContentNextTick() {
+            this.visible = true
+            this.$nextTick(()=> {
+                this.positionContent()
+                document.addEventListener('click', this.onClickDocument)
+            })
+        },
+        positionContent() {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+        },
+        onClickDocument(e) {
+            if (
+                this.$refs.popover &&
+                (this.$refs.popover === e.target || this.$refs.contentWrapper.contains(e.target))
+            ) {
+                return
+            }
+            this.closeContentAndRemoveListener()
+        }
     }
 }
 </script>
