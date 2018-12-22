77600b8c64a3c4eb2406a531b9b2cc7919b5185e
Honohonoho <751718620@qq.com>
2018-12-22 20:54:09 +0800

    优化 Popover 组件: 重构设置 popoverContent 显示位置的代码

--- src/popover.vue
+++ src/popover.vue
@@ -52,24 +52,28 @@ export default {
         positionContent() {
             const {contentWrapper, triggerWrapper} = this.$refs
             document.body.appendChild(contentWrapper)
-            let {width, height, top, left} = triggerWrapper.getBoundingClientRect()
-            if (this.position === 'top') {
-                contentWrapper.style.left = left + window.scrollX + 'px'
-                contentWrapper.style.top = top + window.scrollY + 'px'      
-            }else if(this.position === 'bottom') {
-                contentWrapper.style.left = left + window.scrollX + 'px'
-                contentWrapper.style.top = top + height + window.scrollY + 'px'
-            }else if(this.position === 'left') {
-                contentWrapper.style.left =  left + window.scrollX + 'px'
-                let {height: selfHeight} = contentWrapper.getBoundingClientRect()
-                contentWrapper.style.top = top + window.scrollY + 
-                    (height - selfHeight)/2 + 'px'
-            }else if(this.position === 'right') {
-                contentWrapper.style.left = left + width + window.scrollX + 'px'
-                let {height: selfHeight} = contentWrapper.getBoundingClientRect()
-                contentWrapper.style.top = top + window.scrollY + 
-                    (height - selfHeight)/2 + 'px'
+            const {width, height, top, left} = triggerWrapper.getBoundingClientRect()
+            const {height: selfHeight} = contentWrapper.getBoundingClientRect()
+            let positoinData = {
+                top: {
+                    top: top + window.scrollY,
+                    left: left + window.scrollX
+                },
+                bottom: {
+                    top: top + height + window.scrollY,
+                    left: left + window.scrollX
+                },
+                left: {
+                    top: top + window.scrollY + (height - selfHeight) / 2,
+                    left: left + window.scrollX
+                },
+                right: {
+                    top: top + window.scrollY + (height - selfHeight) / 2,
+                    left: left + width + window.scrollX
+                }
             }
+            contentWrapper.style.top = positoinData[this.position].top + 'px'
+            contentWrapper.style.left = positoinData[this.position].left + 'px'
         },
         onClickDocument(e) {
             if (
