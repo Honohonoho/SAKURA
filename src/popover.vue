9eddcec0f1d9c2a845fdf63ab2a0e0e3b1c3dc20
Honohonoho <751718620@qq.com>
2018-12-22 16:56:03 +0800

    优化 Popover 组件 popoverContent 样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
-        <span ref="triggerWrapper">
+        <span ref="triggerWrapper" class="trigger-wrapper">
             <slot></slot>
         </span>
     </div>
@@ -53,21 +53,56 @@ export default {
             ) {
                 return
             }
+            if (
+                this.$refs.contentWrapper &&
+                (this.$refs.contentWrapper === e.target || this.$refs.contentWrapper.contains(e.target))
+            ) {
+                return
+            }
             this.closeContentAndRemoveListener()
         }
     }
 }
 </script>
 <style lang="scss">
+$border-color: #333;
+$border-radius: 4px;
 .popover {
     display: inline-block;
     vertical-align: top;
     position: relative;
+    .trigger-wrapper {
+        display: inline-block;
+    }
 }
 .content-wrapper {
     position: absolute;
     transform: translateY(-100%);
-    border: 1px solid red;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    filter: drop-shadow(0 0 1px rgba(0, 0, 0, 0.5));
+    background: #fff;
+    padding: .5em 1em;
+    margin-top: -10px;
+    max-width: 20em;
+    word-break: break-all;
+    &::before, &::after {
+        content: '';
+        display: block;
+        border: 10px solid transparent;
+        width: 0;
+        height: 0;
+        position: absolute;
+        left: 10px;
+    }
+    &::before {
+        border-top-color: $border-color;
+        top: calc(100% + 1px);
+    }
+    &::after {
+        border-top-color: #ffffff;
+        top: 100%;
+    }
 }
 </style>
