ff494a227803e84e0e98f0df09a4aded3228026b
Honohonoho <751718620@qq.com>
2018-12-23 14:53:27 +0800

    创建 Popover 组件单元测试

---
+++ test/popover.test.js
@@ -0,0 +1,13 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Popover from '../src/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+    it('存在.', () => {
+        expect(Popover).to.exist
+    })
+    
+})
