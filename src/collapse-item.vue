da7ab1091d86b2e98361bbdbedde7f2d04111e9a
Honohonoho <751718620@qq.com>
2018-12-23 20:53:28 +0800

    完善 Collapse 组件结构及样式

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,11 +1,50 @@
 <template>
-    <slot></slot>
+    <div class="collapse-item">
+        <div class="title">
+            {{title}}
+        </div>
+        <div class="content" v-if="open">
+            <slot></slot>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    name: 'g-collapse-item'
+    name: 'g-collapse-item',
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+            open: false
+        }
+    }
 }
 </script>
 <style lang="scss">
-
+$grey: #ddd;
+$border-radius: 4px;
+.collapse-item {
+    > .title {
+        // border-bottom: 1px solid $grey;
+        border-top: 1px solid $grey;
+        min-height: 32px;
+        display: flex;
+        align-items: center;
+        padding: 0 8px;
+        cursor: pointer;
+    }
+    &:first-child {
+        > .title {
+            border-top: none;
+        }
+    }
+    > .content {
+        padding: 8px;
+        border-top: 1px solid $grey;
+    }
+}
 </style>
--- src/collapse.vue
+++ src/collapse.vue
@@ -1,5 +1,7 @@
 <template>
-    
+    <div class="collapse">
+        <slot></slot>
+    </div>
 </template>
 <script>
 export default {
@@ -7,7 +9,12 @@ export default {
 }
 </script>
 <style lang="scss">
-
+$grey: #ddd;
+$border-radius: 4px;
+.collapse {
+    border: 1px solid $grey;
+    border-radius: $border-radius;
+}
 </style>
 
 
