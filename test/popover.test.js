0b1a44713697bd0bfbe9693903d5744e765b64ec
Honohonoho <751718620@qq.com>
2018-12-23 15:16:39 +0800

    添加 Popover 组件：测试 position 属性的测试用例

--- test/popover.test.js
+++ test/popover.test.js
@@ -9,5 +9,30 @@ describe('Popover', () => {
     it('存在.', () => {
         expect(Popover).to.exist
     })
-    
+    it('可以设置 position', (done) => {
+        Vue.component('g-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+            <g-popover position="bottom" ref="popover">
+                <template slot="content">
+                    <div>内容</div>
+                </template>
+                <template>
+                    <button>bottom点我</button>
+                </template>
+            </g-popover>
+        `
+        const vm = new Vue({
+            el: div
+        })
+        vm.$nextTick(() => {
+            vm.$el.querySelector('button').click()
+            vm.$nextTick(() => {
+                let contentWrapper = vm.$refs.popover.$refs.contentWrapper
+                expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+                done()
+            })
+        })
+    })
 })
