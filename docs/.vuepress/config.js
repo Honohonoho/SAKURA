62a68617b370735792aa1ffbf5a80fb9617d0059
Honohonoho <751718620@qq.com>
2018-12-27 22:12:12 +0800

    添加 UI 组件网站介绍目录

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: 'SAKURA UI 官网',
+    description: '一个好用的 UI 组件库',
+    sidebarDepth: 2,
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '文档', link: '/install/' },
+            { text: 'GitHub', link: 'https://github.com/Honohonoho/lunzi' },
+        ],
+        sidebar: [
+            ['/install/', '安装'],
+            ['/quick_start/', '快速上手'],
+            {
+                title: '组件',
+                collapsable: true,
+                children: [
+                    ['/components/button', 'Button'],
+                ]
+            },
+        ]
+    }
+}
