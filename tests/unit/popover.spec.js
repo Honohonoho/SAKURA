eb015783ea2ee6b7be899849fff257d1044732a5
Honohonoho <751718620@qq.com>
2019-05-26 16:24:27 +0800

    unit test: 改造 popover unit test

--- tests/unit/popover.spec.js
+++ tests/unit/popover.spec.js
@@ -24,32 +24,19 @@ describe('Popover', () => {
         let classes = wrapper.find('.content-wrapper').classes();
         expect(classes).to.include('position-bottom')
     });
-    xit('设置 trigger: click/hover', () => {
-        Vue.component('s-popover', Popover);
-        const div = document.createElement('div');
-        document.body.appendChild(div);
-        div.innerHTML = `
-            <s-popover trigger="hover" ref="popover">
-                <template slot="content">
-                    <div>内容</div>
-                </template>
-                <template>
-                    <button>bottom点我</button>
-                </template>
-            </s-popover>
-        `;
-        const vm = new Vue({
-            el: div
+    it('设置 trigger: click/hover', () => {
+        const wrapper = mount(Popover, {
+            slots: {
+                default: {template: `<button>click me</button>`},
+                content: '<div>content</div>'
+            },
+            propsData: {
+                position: 'bottom',
+                trigger: 'hover'
+            }
         });
-        // 等popover mounted 之后在执行
-        vm.$nextTick(() => {
-            let event = new Event('mouseenter');
-            vm.$el.dispatchEvent(event);
-            vm.$nextTick(() => {
-                let contentWrapper = vm.$refs.popover.$refs.contentWrapper;
-                expect(contentWrapper).to.exist;
-                done()
-            })
-        })
+        expect(wrapper.find('.content-wrapper').element).to.not.exist
+        wrapper.find('.popover').trigger('mouseenter')
+        expect(wrapper.find('.content-wrapper').element).to.exist
     })
 });
