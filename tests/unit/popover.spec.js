a4fdb263c8203908018f505c18079c1c67140032
Honohonoho <751718620@qq.com>
2019-05-26 16:13:40 +0800

    unit test: 改造 popover unit test

--- tests/unit/popover.spec.js
+++ tests/unit/popover.spec.js
@@ -10,36 +10,19 @@ describe('Popover', () => {
     it('存在.', () => {
         expect(Popover).to.exist
     });
-    it('可以设置 position', (done) => {
-        Vue.component('s-popover', Popover);
-        const div = document.createElement('div');
-        document.body.appendChild(div);
+    it('可以设置 position', () => {
         const wrapper = mount(Popover, {
             slots: {
-
+                default: {template: `<button>click me</button>`},
+                content: '<div>content</div>'
+            },
+            propsData: {
+                position: 'bottom'
             }
-        })
-        div.innerHTML = `
-            <s-popover position="bottom" ref="popover">
-                <template slot="content">
-                    <div>内容</div>
-                </template>
-                <template>
-                    <button>bottom点我</button>
-                </template>
-            </s-popover>
-        `;
-        const vm = new Vue({
-            el: div
         });
-        vm.$nextTick(() => {
-            vm.$el.querySelector('button').click();
-            vm.$nextTick(() => {
-                let contentWrapper = vm.$refs.popover.$refs.contentWrapper;
-                expect(contentWrapper.classList.contains('position-bottom')).to.be.true;
-                done()
-            })
-        })
+        wrapper.find('button').trigger('click')
+        let classes = wrapper.find('.content-wrapper').classes();
+        expect(classes).to.include('position-bottom')
     });
     xit('设置 trigger: click/hover', () => {
         Vue.component('s-popover', Popover);
