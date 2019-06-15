8745d650817b415cd9a8bddefde1c18d47e7b053
Honohonoho <751718620@qq.com>
2019-06-15 23:07:57 +0800

    :hammer: 导出组件

---
+++ src/index.js
@@ -0,0 +1,38 @@
+export {default as Button} from './button/button'
+export {default as ButtonGroup} from './button/button-group'
+
+export {default as Input} from './input'
+
+export {default as Icon} from './icon'
+
+export {default as Popover} from './popover'
+
+export {default as Toast} from './toast'
+export {default as Plugin} from './plugin'
+
+export {default as Cascader} from './cascader/cascader'
+export {default as CascaderItem} from './cascader/cascader-item'
+
+export {default as Collapse} from './collapse/collapse'
+export {default as CollapseItem} from './collapse/collapse-item'
+
+export {default as Col} from './grid/col'
+export {default as Row} from './grid/row'
+
+export {default as Header} from './layout/header'
+export {default as Content} from './layout/content'
+export {default as Footer} from './layout/footer'
+export {default as Layout} from './layout/layout'
+export {default as Sider} from './layout/sider'
+
+export {default as Tabs} from './tabs/tabs'
+export {default as TabsBody} from './tabs/tabs-body'
+export {default as TabsHead} from './tabs/tabs-head'
+export {default as TabsItem} from './tabs/tabs-item'
+export {default as TabsPane} from './tabs/tabs-pane'
+
+export {default as Slides} from './slides/slides'
+export {default as SlidesItem} from './slides/slides-item'
+
+export {default as ClickOutside} from './custom_directives'
+
