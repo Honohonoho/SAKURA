25d71711c5e90ef968a42ab8924b092484ff2181
Honohonoho <751718620@qq.com>
2019-06-17 21:30:44 +0800

    Scroll 组件: 迁移代码至scroll.vue

--- src/App.vue
+++ src/App.vue
@@ -136,110 +136,108 @@
         <!--<div class="box">3</div>-->
         <!--</s-slides-item>-->
         <!--</s-slides>-->
-        <div class="parent" ref="parent">
-            <div class="child" ref="child">
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
-                <p>5</p>
-                <p>6</p>
-                <p>7</p>
-                <p>8</p>
-                <p>9</p>
-                <p>10</p>
-                <p>11</p>
-                <p>12</p>
-                <p>13</p>
-                <p>14</p>
-                <p>15</p>
-                <p>16</p>
-                <p>17</p>
-                <p>18</p>
-                <p>19</p>
-                <p>20</p>
-                <p>21</p>
-                <p>22</p>
-                <p>23</p>
-                <p>24</p>
-                <p>25</p>
-                <p>26</p>
-                <p>27</p>
-                <p>28</p>
-                <p>29</p>
-                <p>30</p>
-                <p>31</p>
-                <p>32</p>
-                <p>33</p>
-                <p>34</p>
-                <p>35</p>
-                <p>36</p>
-                <p>37</p>
-                <p>38</p>
-                <p>39</p>
-                <p>40</p>
-                <p>41</p>
-                <p>42</p>
-                <p>43</p>
-                <p>44</p>
-                <p>45</p>
-                <p>46</p>
-                <p>47</p>
-                <p>48</p>
-                <p>49</p>
-                <p>50</p>
-                <p>51</p>
-                <p>52</p>
-                <p>53</p>
-                <p>54</p>
-                <p>55</p>
-                <p>56</p>
-                <p>57</p>
-                <p>58</p>
-                <p>59</p>
-                <p>60</p>
-                <p>61</p>
-                <p>62</p>
-                <p>63</p>
-                <p>64</p>
-                <p>65</p>
-                <p>66</p>
-                <p>67</p>
-                <p>68</p>
-                <p>69</p>
-                <p>70</p>
-                <p>71</p>
-                <p>72</p>
-                <p>73</p>
-                <p>74</p>
-                <p>75</p>
-                <p>76</p>
-                <p>77</p>
-                <p>78</p>
-                <p>79</p>
-                <p>80</p>
-                <p>81</p>
-                <p>82</p>
-                <p>83</p>
-                <p>84</p>
-                <p>85</p>
-                <p>86</p>
-                <p>87</p>
-                <p>88</p>
-                <p>89</p>
-                <p>90</p>
-                <p>91</p>
-                <p>92</p>
-                <p>93</p>
-                <p>94</p>
-                <p>95</p>
-                <p>96</p>
-                <p>97</p>
-                <p>98</p>
-                <p>99</p>
-                <p>100</p>
-            </div>
-        </div>
+        <s-scroll style="width: 400px;height: 400px;">
+            <p>1</p>
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
+            <p>7</p>
+            <p>8</p>
+            <p>9</p>
+            <p>10</p>
+            <p>11</p>
+            <p>12</p>
+            <p>13</p>
+            <p>14</p>
+            <p>15</p>
+            <p>16</p>
+            <p>17</p>
+            <p>18</p>
+            <p>19</p>
+            <p>20</p>
+            <p>21</p>
+            <p>22</p>
+            <p>23</p>
+            <p>24</p>
+            <p>25</p>
+            <p>26</p>
+            <p>27</p>
+            <p>28</p>
+            <p>29</p>
+            <p>30</p>
+            <p>31</p>
+            <p>32</p>
+            <p>33</p>
+            <p>34</p>
+            <p>35</p>
+            <p>36</p>
+            <p>37</p>
+            <p>38</p>
+            <p>39</p>
+            <p>40</p>
+            <p>41</p>
+            <p>42</p>
+            <p>43</p>
+            <p>44</p>
+            <p>45</p>
+            <p>46</p>
+            <p>47</p>
+            <p>48</p>
+            <p>49</p>
+            <p>50</p>
+            <p>51</p>
+            <p>52</p>
+            <p>53</p>
+            <p>54</p>
+            <p>55</p>
+            <p>56</p>
+            <p>57</p>
+            <p>58</p>
+            <p>59</p>
+            <p>60</p>
+            <p>61</p>
+            <p>62</p>
+            <p>63</p>
+            <p>64</p>
+            <p>65</p>
+            <p>66</p>
+            <p>67</p>
+            <p>68</p>
+            <p>69</p>
+            <p>70</p>
+            <p>71</p>
+            <p>72</p>
+            <p>73</p>
+            <p>74</p>
+            <p>75</p>
+            <p>76</p>
+            <p>77</p>
+            <p>78</p>
+            <p>79</p>
+            <p>80</p>
+            <p>81</p>
+            <p>82</p>
+            <p>83</p>
+            <p>84</p>
+            <p>85</p>
+            <p>86</p>
+            <p>87</p>
+            <p>88</p>
+            <p>89</p>
+            <p>90</p>
+            <p>91</p>
+            <p>92</p>
+            <p>93</p>
+            <p>94</p>
+            <p>95</p>
+            <p>96</p>
+            <p>97</p>
+            <p>98</p>
+            <p>99</p>
+            <p>100</p>
+        </s-scroll>
     </div>
 </template>
 
@@ -269,6 +267,7 @@
     // import city from './city';
     import Slides from './slides/slides';
     import SlidesItem from './slides/slides-item';
+    import Scroll from './scroll'
 
     Vue.use(plugin);
 
@@ -319,6 +318,7 @@
             // 's-collapse': Collapse,
             // 's-collapse-item': CollapseItem,
             // 's-cascader': Cascader
+            's-scroll': Scroll
         },
         data() {
             return {
@@ -341,45 +341,7 @@
             // }, 2000)
         },
         mounted() {
-            let parent = this.$refs.parent
-            let child = this.$refs.child
-            child.style.transition = `transform 0.1s ease`
-            let translateY = 0
 
-            // 计算什么时候滑到底部了...
-            let {height: childHeight} = child.getBoundingClientRect()
-            let {height: parentHeight} = parent.getBoundingClientRect()
-            let {borderTopWidth, borderBottomWidth, paddingTop, paddingBottom} = window.getComputedStyle(parent)
-            borderTopWidth = parseInt(borderTopWidth)
-            borderBottomWidth = parseInt(borderBottomWidth)
-            paddingTop = parseInt(paddingTop)
-            paddingBottom = parseInt(paddingBottom)
-            // 最大可滑动距离
-            let maxHeight = childHeight - parentHeight + (borderTopWidth + borderBottomWidth + paddingTop + paddingBottom)
-
-            parent.addEventListener('wheel', (e) => {
-                // 限制最大滑动距离，要不然滑的快滚得快
-                if (e.deltaY > 20) {
-                    translateY -= 20 * 3
-                } else if (e.deltaY < -20) {
-                    translateY -= -20 * 3
-                } else {
-                    translateY -= e.deltaY * 3
-                }
-
-                if (translateY > 0) {
-                    // 到顶部了，不能再滑了
-                    translateY = 0
-                } else if (translateY < -maxHeight) {
-                    // 到低部了，不能再滑了
-                    translateY = -maxHeight
-                }
-                child.style.transform = `translateY(${translateY}px)`
-            })
-            // 兼容移动端
-            // parent.addEventListener('touchmove', () => {
-            //     console.log('touchmove');
-            // })
         },
         methods: {
             // onSelectedChanged(itemName) {
@@ -460,14 +422,5 @@
         list-style: noe;
         padding: 0;
         margin: 0;
-    }
-    .parent {
-        width: 400px;
-        height: 400px;
-        overflow: hidden;
-        border: 5px solid red;
-        .child {
-            border: 5px solid blue;
-        }
     }
 </style>
--- src/scroll.vue
+++ src/scroll.vue
@@ -1,13 +1,64 @@
 <template>
-    
+    <div class="s-scroll-wrapper" ref="parent">
+        <div class="s-scroll" ref="child">
+            <slot></slot>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "scroll"
+        name: "s-scroll",
+        mounted() {
+            let parent = this.$refs.parent
+            let child = this.$refs.child
+            let translateY = 0
+
+            // 计算什么时候滑到底部了...
+            let {height: childHeight} = child.getBoundingClientRect()
+            let {height: parentHeight} = parent.getBoundingClientRect()
+            let {borderTopWidth, borderBottomWidth, paddingTop, paddingBottom} = window.getComputedStyle(parent)
+            borderTopWidth = parseInt(borderTopWidth)
+            borderBottomWidth = parseInt(borderBottomWidth)
+            paddingTop = parseInt(paddingTop)
+            paddingBottom = parseInt(paddingBottom)
+            // 最大可滑动距离
+            let maxHeight = childHeight - parentHeight + (borderTopWidth + borderBottomWidth + paddingTop + paddingBottom)
+
+            parent.addEventListener('wheel', (e) => {
+                // 限制最大滑动距离，要不然滑的快滚得快
+                if (e.deltaY > 20) {
+                    translateY -= 20 * 3
+                } else if (e.deltaY < -20) {
+                    translateY -= -20 * 3
+                } else {
+                    translateY -= e.deltaY * 3
+                }
+
+                if (translateY > 0) {
+                    // 到顶部了，不能再滑了
+                    translateY = 0
+                } else if (translateY < -maxHeight) {
+                    // 到低部了，不能再滑了
+                    translateY = -maxHeight
+                }
+                child.style.transform = `translateY(${translateY}px)`
+            })
+            // 兼容移动端
+            // parent.addEventListener('touchmove', () => {
+            //     console.log('touchmove');
+            // })
+        }
     }
 </script>
 
 <style scoped lang="scss">
-
+    .s-scroll-wrapper {
+        overflow: hidden;
+        border: 5px solid red;
+       .s-scroll {
+           border: 5px solid blue;
+           transition: transform 0.05s ease;
+       }
+    }
 </style>
