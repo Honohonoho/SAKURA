4b91eecd161579f8c4b209bd204883ee740a0035
Honohonoho <751718620@qq.com>
2019-06-20 20:44:58 +0800

    Scroll 组件: 迁移拖拽功能代码至 Scroll 组件

--- src/scroll.vue
+++ src/scroll.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="s-scroll-wrapper" ref="parent" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
-        <div class="s-scroll" ref="child">
+        <div class="s-scroll" ref="child" :style="{transform: `translateY(${this.contentTranslateY}px)`}">
             <slot></slot>
         </div>
         <div class="s-scroll-track" v-show="scrollBarVisible">
-            <div class="s-scroll-bar" ref="bar">
+            <div class="s-scroll-bar" ref="bar"
+                 @mousedown="onMouseDownInScrollBar"
+                 @mousemove="onMouseMoveInScrollBar"
+                 @mouseup="onMouseUpInScrollBar"
+                 @selectstart="onSelectStartInScrollBar"
+            >
                 <div class="s-scroll-bar-inner"></div>
             </div>
         </div>
@@ -14,19 +19,38 @@
 <script>
     export default {
         name: "s-scroll",
-        data(){
+        data() {
             return {
-                scrollBarVisible: false
+                scrollBarVisible: false,
+                parentHeight: undefined, // 内容区容器高度
+                childHeight: undefined, // 内容区高度
+                contentTranslateY: 0, // 内容区移动距离
+                barHeight: undefined, // 滚动条高度
+                isScrolling: false,
+                startPosition: undefined,
+                endPosition: undefined,
+                scrollBarTranslateY: 0 // 滚动条移动距离
             }
         },
+        watch: {
+        },
         mounted() {
+            document.addEventListener('mousemove', (e)=> {
+                this.onMouseMoveInScrollBar(e)
+            })
+            document.addEventListener('mouseup', (e)=>{
+                this.onMouseUpInScrollBar(e)
+            })
+
             let parent = this.$refs.parent
             let child = this.$refs.child
-            let translateY = 0
+            let contentTranslateY = 0
 
             // 计算什么时候滑到底部了...
             let {height: childHeight} = child.getBoundingClientRect()
             let {height: parentHeight} = parent.getBoundingClientRect()
+            this.parentHeight = parentHeight
+            this.childHeight = childHeight
             let {borderTopWidth, borderBottomWidth, paddingTop, paddingBottom} = window.getComputedStyle(parent)
             borderTopWidth = parseInt(borderTopWidth)
             borderBottomWidth = parseInt(borderBottomWidth)
@@ -38,49 +62,92 @@
             parent.addEventListener('wheel', (e) => {
                 // 限制最大滑动距离，要不然滑的快滚得快
                 if (e.deltaY > 20) {
-                    translateY -= 20 * 3
+                    this.contentTranslateY -= 20 * 3
                 } else if (e.deltaY < -20) {
-                    translateY -= -20 * 3
+                    this.contentTranslateY -= -20 * 3
                 } else {
-                    translateY -= e.deltaY * 3
+                    this.contentTranslateY -= e.deltaY * 3
                 }
 
-                if (translateY > 0) {
+                if (this.contentTranslateY > 0) {
                     // 到顶部了，不能再滑了
-                    translateY = 0
-                } else if (translateY < -maxHeight) {
+                    this.contentTranslateY = 0
+                } else if (this.contentTranslateY < -maxHeight) {
                     // 到低部了，不能再滑了
-                    translateY = -maxHeight
+                    this.contentTranslateY = -maxHeight
                 } else {
                     // 如果内部内容滚到顶部或底部，则滚动外面页面
                     e.preventDefault()
                 }
-                child.style.transform = `translateY(${translateY}px)`
-                this.calcScrollBarHeight(parentHeight, childHeight, translateY)
+                this.calcScrollBarHeight(parentHeight, childHeight, this.contentTranslateY)
             })
             // 兼容移动端
             // parent.addEventListener('touchmove', () => {
             //     console.log('touchmove');
             // })
-            this.calcScrollBarHeight(parentHeight, childHeight, translateY)
+            this.calcScrollBarHeight(parentHeight, childHeight, this.contentTranslateY)
+
+        },
+        beforeDestroy() {
+          // 移除document listener
         },
         methods: {
-            // 计算scroll bar高度
-            calcScrollBarHeight (parentHeight, childHeight, translateY){
-                // bar 的高度可以通过等比关系计算出来
+            // 计算scroll bar高度和移动的距离
+            calcScrollBarHeight(parentHeight, childHeight, translateY) {
+                // bar 的高度可以通过等比关系计算出来 ↓↓↓
                 // barHeight / parentHeight = parentHeight / childHeight
                 let barHeight = parentHeight * parentHeight / childHeight
+                this.barHeight = barHeight
                 let bar = this.$refs.bar
                 bar.style.height = barHeight + 'px'
                 // deltaHeight: bar滚动距离可以通过等比关系计算出来
                 let deltaHeight = parentHeight * translateY / childHeight
                 bar.style.transform = `translateY(${-deltaHeight}px)`
+                // 同步 滚轮滚动 和 拖拽滚动条 的移动距离
+                this.scrollBarTranslateY = -deltaHeight
             },
-            onMouseEnter () {
+            onMouseEnter() {
                 this.scrollBarVisible = true
             },
-            onMouseLeave () {
+            onMouseLeave() {
                 this.scrollBarVisible = false
+                this.isScrolling = false
+            },
+            onMouseDownInScrollBar(e) {
+                this.isScrolling = true
+                let {screenX, screenY} = e
+                this.startPosition = {x:screenX, y:screenY}
+            },
+            onMouseMoveInScrollBar(e) {
+                if (!this.isScrolling) { return }
+                let {screenX, screenY} = e
+
+                // scroll bar 最大移动距离，防止移出边界
+                let barMaxScrollHeight = this.parentHeight - this.barHeight
+
+                this.endPosition = {x:screenX, y:screenY}
+                let delta = {x: this.endPosition.x - this.startPosition.x, y: this.endPosition.y - this.startPosition.y}
+                // 只用纵向移动
+                // translateX = parseInt(translateX) + delta.x
+                this.scrollBarTranslateY = parseInt(this.scrollBarTranslateY) + delta.y
+
+                if(this.scrollBarTranslateY < 0) {
+                    this.scrollBarTranslateY = 0
+                } else if(this.scrollBarTranslateY > barMaxScrollHeight) {
+                    this.scrollBarTranslateY = barMaxScrollHeight
+                }
+                // 同步 滚轮滚动 和 拖拽滚动条 的移动距离
+                this.contentTranslateY = -(this.childHeight * this.scrollBarTranslateY) / this.parentHeight
+
+                this.startPosition = this.endPosition
+                this.$refs.bar.style.transform = `translate(0px, ${parseInt(this.scrollBarTranslateY) + delta.y}px)`
+            },
+            onMouseUpInScrollBar() {
+                this.isScrolling = false
+            },
+            // 防止滚动时选中
+            onSelectStartInScrollBar(e) {
+                e.preventDefault()
             }
         }
     }
@@ -91,9 +158,9 @@
         overflow: hidden;
         border: 1px solid red;
         position: relative;
-       .s-scroll {
-           transition: transform 0.05s ease;
-       }
+        .s-scroll {
+            transition: transform 0.05s ease;
+        }
         .s-scroll-track {
             position: absolute;
             top: 0;
