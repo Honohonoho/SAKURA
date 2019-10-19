2e3f3106007a7e697cdfcb75689d6592096b2041
Honohonoho <751718620@qq.com>
2019-10-19 20:51:38 +0800

    Icon 组件: 添加新 icon

--- src/icon.vue
+++ src/icon.vue
@@ -9,7 +9,10 @@
     export default {
         name: 's-icon',
         props: {
-            name: {}
+            name: {
+                type: String,
+                default: ''
+            }
         }
     }
 </script>
